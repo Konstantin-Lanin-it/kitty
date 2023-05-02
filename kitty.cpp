56958b4a85514a5220c9f2e9f2ab24e61b675bcc
Konstantin-Lanin-it <127977203+Konstantin-Lanin-it@users.noreply.github.com>
2023-05-02 23:57:46 +0300

    Create kitty.cpp

---
+++ kitty.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << ".∧＿∧" << endl;
+    cout << "( ･ω･｡)つ━☆・*。" << endl;
+    cout << "⊂　 ノ 　　　・゜+." << endl;
+    cout << "しーＪ　　　°。+ *´¨)" << endl;
+    cout << "　　    　　　.· ´¸.·*´¨) ¸.·*¨)" << endl;
+    cout << "　　　　　　　　　.· ´¸.·*´¨) ¸.·*¨)" << endl;
+    cout << "　　　　　　　　　　(¸.·´ (¸.·'* ☆" << endl;
+}
